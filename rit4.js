5bf0ea36be7cc60d4e6c4273d61cc325583888c8
jain-ritik <44414761+jain-ritik@users.noreply.github.com>
2018-11-22 09:33:28 +0530

    Update rit4.js

--- rit4.js
+++ rit4.js
@@ -3,12 +3,55 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const writeStream = fs.createWriteStream('post.csv');
 const obj = {
-		table:[]
-		};
+        table:[]
+        };
+
+
+var name;
+var name1;
+var name2;
+var A,B,C;
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+readline.question(`enter city name?`, (name) => {
+  console.log(`Hi ${name}!`)
+readline.close()
+A=name;
+ sam();
+})
+const sam = function(){
+const readline1 = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+readline1.question(`enter state name?`, (name1) => {
+  console.log(`Hi ${name1}!`)
+B=name1;
+readline1.close()
+ dep();
+})}
+const dep = function(){
+const readline2 = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+readline2.question(`enter country name?`, (name2) => {
+  console.log(`Hi ${name2}!`)
+C=name2;
+readline2.close()
+rit()
+})}
 // Write Headers
 //writeStream.write(`Title\n`);
-
-request('https://stackoverflow.com/jobs', (error, response, html) => {
+const rit = function(){
+console.log(`https://stackoverflow.com/jobs?l=${A}%2C+${B}%2C+${C}&d=20&u=Km`)
+request(`https://stackoverflow.com/jobs?l=${A}%2C+${B}%2C+${C}&d=20&u=Km`, (error, response, html) => {
   if (!error && response.statusCode == 200) {
     const $ = cheerio.load(html);
     $('.-job-summary').each((i, el) => {
@@ -16,20 +59,21 @@ request('https://stackoverflow.com/jobs', (error, response, html) => {
         .find('.-title')
         .text()
         .replace(/\s\s+/g, '');
-console.log(title);
+//console.log(title);
 const l = $(el)
       .find('.fc-black-500')
       .text()
       .replace(/\s\s+/g, '');
-console.log(l);  
+//console.log(l); 
 obj.table.push({"name": title, "details": l});
-console.log('\n');          
+//console.log('\n');         
       // Write Row To CSV
     //  writeStream.write(`${title}, ${link}, ${date} \n`);
 //writeStream.write(`${title}\n`);
     });
-    console.log('Scraping Done...');
+   // console.log('Scraping Done...');
     const json = JSON.stringify(obj);
+console.log(obj);
     fs.writeFile('myjsonfile.json', json, 'utf8');
   }
-});
+});}
